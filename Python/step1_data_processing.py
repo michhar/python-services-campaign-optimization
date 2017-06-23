debb39d8142ce3dceb3e842566687c8e3fc65be6
michhar <michhar@users.noreply.github.com>
2017-06-22 18:10:41 -0700

    dummied vars and using as factors to lin reg

--- Python/step1_data_processing.py
+++ Python/step1_data_processing.py
@@ -14,10 +14,14 @@ Prerequisites:
       with 'virtualenv <python version path>' and 'activate' to begin using)
   - Command line or equivalent access
 
-What these scripts do:
-* Reads data into dataframe
-* Imports dataframe to SQL Server Campaign database
-* Processes in-database with revoscalepy stats and ML methods
+What this script does:
+* Reads data into dataframe and dummies categorical variables
+* Imports dataframe to SQL Server table in established database
+* In-dabase processing and linear regression with revoscalepy
+* In-memory linear regression with scikit-learn
+
+TODO: compare in-database to local compute context
+
 * Report metrics
 * Visualize with Python plotting tools
 
@@ -28,22 +32,26 @@ from revoscalepy.computecontext.RxComputeContext import  RxComputeContext
 from revoscalepy.computecontext.RxInSqlServer import RxSqlServerData, RxInSqlServer
 from revoscalepy.etl.RxImport import rx_import_datasource
 # from revoscalepy.etl.RxImport import RxDataSource
-from revoscalepy.functions.RxDataStep import rx_data_step_ex
+# from revoscalepy.functions.RxDataStep import rx_data_step_ex
 from revoscalepy.functions.RxLinMod import rx_lin_mod_ex
-from revoscalepy.functions.RxLogit import rx_logit_ex
+# from revoscalepy.functions.RxLogit import rx_logit_ex
 from revoscalepy.functions.RxPredict import rx_predict_ex
 from revoscalepy.functions.RxSummary import rx_summary
-from revoscalepy.utils.RxOptions import RxOptions
+# from revoscalepy.utils.RxOptions import RxOptions
 from sklearn.linear_model import LinearRegression
 
-from config import CONNECTION_STRING, BASE_DIR, LOCAL
+from config import CONNECTION_STRING, BASE_DIR
 import os
 import pandas as pd
 from pprint import pprint
 
-def main(tablename, csvfile, overwrite=False):
-    """Imports a csv into SQL Server table and performs linear 
-       regression test.
+def main(tablename, inputdf, overwrite=False):
+    """Imports a DataFrame into SQL Server table and, operating
+       in-database, uses linear regression to create a
+       predictive model.
+
+       A comparison to an out-of-database, in memory method
+       is performed.
        
        Parameters
        ----------
@@ -51,12 +59,12 @@ def main(tablename, csvfile, overwrite=False):
        tablename : str
            The new or previosly create table name in database.
 
-       csvfile : str
-           The csv file with data for import (full path).
+       inputdf : pandas.DataFrame
+           The DataFrame with data for import.
 
        overwrite : bool
-           Whether or not to overwrite the table.  Set to
-           True if this is a new table.
+           Whether or not to overwrite the table.  
+           Set to True if this is a new table.
        """
 
     ####################################################################
@@ -75,14 +83,6 @@ def main(tablename, csvfile, overwrite=False):
     if overwrite:
 
         ####################################################################
-        # Read in data into a pandas df from a file
-        ####################################################################
-
-        # TODO:  look into dask for holding chunks of data for import
-        # Create the file path to the csv data
-        input_df = pd.read_csv(csvfile)
-
-        ####################################################################
         # Create table in SQL server
         ####################################################################
 
@@ -97,19 +97,12 @@ def main(tablename, csvfile, overwrite=False):
 
         print("Reading data into tables...")
 
-        # This is not working unfortunately:
-        #help(RxDataSource)
-        #data = RxDataSource(open(os.path.join(file_path, 'Campaign_Detail.csv'), 'r'))
-
-        # The method rx_import_datasource expects a pandas df or an 
-        #   RxDataSource
-
-        rx_import_datasource(inData=input_df, \
+        rx_import_datasource(inData=inputdf, \
             outFile=outfile)
 
         # Right now can only run once because overwrite is not working
-        # rx_import_datasource(inData=campaign_detail_df, \
-        #     outFile=Campaign_Detail, overwrite=True)
+        # rx_import_datasource(inData=inputdf, \
+        #     outFile=outfile, overwrite=True)
 
         # NB: overwrite param not accepting bool values so this can only be run once righ now!
 
@@ -119,62 +112,31 @@ def main(tablename, csvfile, overwrite=False):
 
     # Train data
     data_source_train = RxSqlServerData(
-        sqlQuery = "SELECT TOP 10000 * FROM Lead_Demography_Tbl ORDER BY Lead_Id", 
+        sqlQuery = "SELECT TOP 10000 * FROM Lead_Demography_Tbl \
+                    ORDER BY Lead_Id", 
         connectionString = CONNECTION_STRING,
         colInfo = { # NB: may want to add all cols here
             "No_Of_Children" : { "type" : "integer" },
             "Household_Size" : { "type" : "integer" },
-            "No_Of_Dependents" : { "type" : "integer" },
-            "Highest_Education" : { 
-                "type" : "factor",
-            #     "levels" : [
-            #         "High School",
-            #         "College",
-            #         "Attended Vocational",
-            #         "Graduate School"
-            #         ]
-                },
-            "Annual_Income_Bucket" : { 
-                "type" : "factor",
-                # "levels" : [ 
-                #     "60k-120k",
-                #     ">120k",
-                #     "<60k"
-                #     ]
-                }
+            "No_Of_Dependents" : { "type" : "integer" }
             }
         )
 
     # Import training data RxImport style from new query source       
     X_y_train = rx_import_datasource(data_source_train)
-    print(X_y_train)
+    # print(X_y_train)
 
     # Test data (let's pick ~30% size of training dataset)
     data_source_test = RxSqlServerData(
-        sqlQuery = "SELECT * FROM Lead_Demography_Tbl ORDER BY Lead_Id OFFSET 10000 ROWS FETCH FIRST 3000 ROW ONLY", 
+        sqlQuery = "SELECT * FROM Lead_Demography_Tbl \
+                    ORDER BY Lead_Id \
+                    OFFSET 10000 ROWS \
+                    FETCH FIRST 3000 ROW ONLY", 
         connectionString = CONNECTION_STRING,
         colInfo = { # NB: may want to add all cols here
             "No_Of_Children" : { "type" : "integer" },
             "Household_Size" : { "type" : "integer" },
-            "No_Of_Dependents" : { "type" : "integer" },
-            "Highest_Education" : { 
-                "type" : "factor",
-                # "levels" : [
-                #     "",
-                #     "High School",
-                #     "College",
-                #     "Attended Vocational",
-                #     "Graduate School"
-                #     ]
-                },
-            "Annual_Income_Bucket" : { 
-                "type" : "factor",
-                # "levels" : [ 
-                #     "60k-120k",
-                #     ">120k",
-                #     "<60k"
-                #     ]
-                }
+            "No_Of_Dependents" : { "type" : "integer" }
             }
         )
 
@@ -185,17 +147,19 @@ def main(tablename, csvfile, overwrite=False):
     # Run revoscalepy linear regression and summary (in-database)
     #####################################################################
 
-    mod = rx_lin_mod_ex(formula="No_Of_Children ~ F(Annual_Income_Bucket)+F(Highest_Education)", 
+    mod = rx_lin_mod_ex(formula="No_Of_Children ~ \
+                                F(Highest_Education_High_School)+\
+                                F(Annual_Income_Bucket_lt60k)", 
         data=X_y_train, compute_context=compute_context)
     assert mod is not None
     assert mod._results is not None
     pprint(mod._results)
-    summary = rx_summary(formula="No_Of_Children ~ F(Annual_Income_Bucket)+F(Highest_Education)", 
+    summary = rx_summary(formula="No_Of_Children ~ \
+                                F(Highest_Education_High_School)+\
+                                F(Annual_Income_Bucket_lt60k)", 
         data=X_y_train, compute_context=compute_context)
     # print(summary) # NB: Doesn't want to print
     
-    # pprint(vars(mod))
-
     #####################################################################
     # Run scikit-learn linear regression (in-memory)
     #####################################################################
@@ -205,7 +169,30 @@ def main(tablename, csvfile, overwrite=False):
 
 if __name__ == '__main__':
 
+
+    ####################################################################
+    # Read in data into a pandas df from a file (do here for manipulation)
+    ####################################################################
+
     file_path = os.path.join(BASE_DIR, 'Data')
     inputfile = os.path.join(file_path, "Lead_Demography.csv")
 
-    main(tablename="Lead_Demography_Tbl", csvfile=inputfile, overwrite=False)
+    # TODO:  look into dask for holding chunks of data for import
+    # Create the file path to the csv data
+    input_df = pd.read_csv(inputfile)
+
+    ####################################################################
+    # Dummy encode variables of interest
+    ####################################################################
+
+    df_dummy = pd.get_dummies(input_df, 
+        columns=['Annual_Income_Bucket', 'Highest_Education'])
+
+    # Fix column names
+    new_cols = [x.replace(' ', '_').replace('>', 'gt').replace('<', 'lt') 
+        for x in df_dummy.columns]
+    print(new_cols)
+    df_dummy.columns = new_cols
+
+    main(tablename="Lead_Demography_Tbl", inputdf=df_dummy, 
+        overwrite=True)
