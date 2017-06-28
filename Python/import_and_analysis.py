208873dbb9f85236c0fbdc25a6dd02dba8ec37d1
michhar <michhar@users.noreply.github.com>
2017-06-28 13:34:03 -0700

    now log reg and added accuracy scoring

--- Python/import_and_analysis.py
+++ Python/import_and_analysis.py
@@ -25,6 +25,8 @@ TODO: compare in-database to local compute context
 * Report metrics
 * Visualize with Python plotting tools
 
+Based on: https://docs.microsoft.com/en-us/sql/advanced-analytics/tutorials/use-python-revoscalepy-to-create-model
+
 Micheleen Harris
 """
 
@@ -35,19 +37,22 @@ from revoscalepy.computecontext.RxInSqlServer import RxOdbcData
 from revoscalepy.etl.RxImport import rx_import_datasource
 from revoscalepy.datasource.RxXdfData import RxXdfData
 from revoscalepy.datasource.RxFileData import RxFileData
-# from revoscalepy.etl.RxImport import RxDataSource
 from revoscalepy.functions.RxDataStep import rx_data_step_ex
 from revoscalepy.functions.RxLinMod import rx_lin_mod_ex
-# from revoscalepy.functions.RxLogit import rx_logit_ex
+from revoscalepy.functions.RxLogit import rx_logit_ex
 from revoscalepy.functions.RxPredict import rx_predict_ex
 from revoscalepy.functions.RxSummary import rx_summary
-# from revoscalepy.utils.RxOptions import RxOptions
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+from sklearn.preprocessing import binarize
 from sklearn.linear_model import LinearRegression
 
 from config import CONNECTION_STRING, BASE_DIR
 import os
 import pandas as pd
+import numpy as np
+import dask.dataframe as dd
 from pprint import pprint
+
 
 def main(tablename, inputdf, overwrite=False):
     """Imports a DataFrame into SQL Server table and, operating
@@ -66,9 +71,9 @@ def main(tablename, inputdf, overwrite=False):
        inputdf : pandas.DataFrame
            The DataFrame with data for import.
 
-       overwrite : bool
+       overwrite : bool, optional (default=False)
            Whether or not to overwrite the table.  
-           Set to True if this is a new table.
+           Set to True if this is a new table, otherwise False.
        """
 
     ####################################################################
@@ -119,16 +124,13 @@ def main(tablename, inputdf, overwrite=False):
         sqlQuery = "SELECT TOP 10000 * FROM Lead_Demography_Tbl \
                     ORDER BY Lead_Id", 
         connectionString = CONNECTION_STRING,
-        colInfo = { # NB: may want to add all cols here
+        colInfo = {
             "No_Of_Children" : { "type" : "integer" },
-            "Household_Size" : { "type" : "integer" },
-            "No_Of_Dependents" : { "type" : "integer" }
             }
         )
 
     # Import training data RxImport style from new query source       
     X_y_train = rx_import_datasource(data_source_train)
-    # print(X_y_train)
 
     # Test data (let's pick ~30% size of training dataset)
     data_source_test = RxSqlServerData(
@@ -137,10 +139,10 @@ def main(tablename, inputdf, overwrite=False):
                     OFFSET 10000 ROWS \
                     FETCH FIRST 3000 ROW ONLY", 
         connectionString = CONNECTION_STRING,
-        colInfo = { # NB: may want to add all cols here
+        colInfo = {
             "No_Of_Children" : { "type" : "integer" },
-            "Household_Size" : { "type" : "integer" },
-            "No_Of_Dependents" : { "type" : "integer" }
+            "Annual_Income_Bucket_gt120k" : {"typte" : "factor"},
+            "Highest_Education_Graduate_School" : {"typte" : "factor"}
             }
         )
 
@@ -151,10 +153,10 @@ def main(tablename, inputdf, overwrite=False):
     # Run revoscalepy linear regression on training data (in-database)
     #####################################################################
 
-    mod = rx_lin_mod_ex(formula="No_Of_Children ~ \
-                                F(Highest_Education_High_School)+\
-                                F(Annual_Income_Bucket_lt60k)", 
-        data=X_y_train, compute_context=compute_context)
+    mod = rx_logit_ex(formula="Annual_Income_Bucket_gt120k ~ \
+                                F(Highest_Education_Graduate_School)", 
+        data=X_y_train, compute_context=compute_context, verbose=2)
+        # cov_coef=True)
     assert mod is not None
     assert mod._results is not None
     pprint(mod._results)
@@ -165,22 +167,54 @@ def main(tablename, inputdf, overwrite=False):
 
     # Note:  for "data" use data source and not the rximport'ed data
 
-    summary = rx_summary(formula="No_Of_Children ~ \
-                                F(Highest_Education_High_School)+\
-                                F(Annual_Income_Bucket_lt60k)", 
-        data=data_source_test, compute_context=compute_context)
-    print(summary)
+    print('\nSummary: \n')
+
+    summary = rx_summary("F(Annual_Income_Bucket_gt120k) ~ \
+                                F(Highest_Education_Graduate_School)", 
+        data=data_source_train, compute_context=compute_context,
+        cube=True, verbose=2)
+
+    #####################################################################
+    # Predict on test data (in-database)
+    #####################################################################
+
+    print("\nPredict on test: \n")
+
+    pred = rx_predict_ex(mod, data=X_y_test, verbose=2, write_model_vars=True)
+
+    #####################################################################
+    # Metrics for predition based on groundtruth (with scikit-learn tools)
+    #####################################################################  
+
+    pred_results = pred._results['Annual_Income_Bucket_gt120k_Pred']
+    # For some reason the prediction results are not in a binary [0,1] format
+    y_pred = binarize(pred_results, threshold=(min(pred_results) + max(pred_results))/2).reshape(-1, 1)
+    y_true = pred._data['Annual_Income_Bucket_gt120k']
+
+    
+    print("Model prediction results:", y_pred)
+    print("Actual values: ", y_true)
+
+    print("Accuracy score: ", accuracy_score(
+                        y_true=y_true, 
+                        y_pred=y_pred))
+
+
+
+    # # NB: If this is regression (rx_lin_mod_ex) can look at metrics like:
+    # print("R^2 (coefficient of determination): ", r2_score(
+    #                     y_true=y_true, 
+    #                     y_pred=y_pred))
+    
     
     #####################################################################
     # Run scikit-learn linear regression (in-memory) - TODO
     #####################################################################
 
-    df_train = pd.DataFrame(X_y_train)
-    df_test = pd.DataFrame(X_y_test)
+    # df_train = pd.DataFrame(X_y_train)
+    # df_test = pd.DataFrame(X_y_test)
 
 if __name__ == '__main__':
-    
-    import dask.dataframe as dd
 
     ####################################################################
     # Read in data into a pandas df from a file (do here for manipulation)
@@ -188,18 +222,32 @@ if __name__ == '__main__':
 
     file_path = os.path.join(BASE_DIR, 'Data')
     # Note: converted the 'Lead_Id' column to integer by removing chars
-    inputfile = os.path.join(file_path, "Lead_Demography_withID.csv")
-    # inputfile = os.path.join(file_path, "Lead_Demography.csv")
+    inputfile = os.path.join(file_path, "Lead_Demography.csv")
 
     # Create the file path to the csv data (could use dask to do some 
-    #   preprocessing - see "Dask way")
+    #   preprocessing out-of-core - see "Dask way")
     input_df = pd.read_csv(inputfile)
 
     ####################################################################
-    # Dask way
+    # Dummy encode variables of interest with pandas
     ####################################################################
 
-    # Read a csv file into a dask dataframe (data chunked on disk)
+    df_dummy = pd.get_dummies(input_df, 
+        columns=['Annual_Income_Bucket', 'Highest_Education'])
+
+    # Fix column names
+    new_cols = [x.replace(' ', '_').replace('>', 'gt').replace('<', 'lt') 
+        for x in df_dummy.columns]
+    print(new_cols)
+    df_dummy.columns = new_cols
+
+    ####################################################################
+    # Dask way - optional perf speedup and work in progress
+    ####################################################################
+
+    # # Read a csv file into a dask dataframe (data chunked on disk) 
+    # #   - must have numeric id, and dummy variables
+
     # input_df = dd.read_csv(inputfile, dtype={'Annual_Income_Bucket' : 'category',
     #     'Highest_Education' : 'category'})
 
@@ -211,18 +259,6 @@ if __name__ == '__main__':
     # df_dummy.to_csv(dummyfile)
     # print(dummyfile)
 
-    ####################################################################
-    # Dummy encode variables of interest with pandas
-    ####################################################################
-
-    # df_dummy = pd.get_dummies(input_df, 
-    #     columns=['Annual_Income_Bucket', 'Highest_Education'])
-
-    # Fix column names
-    # new_cols = [x.replace(' ', '_').replace('>', 'gt').replace('<', 'lt') 
-    #     for x in df_dummy.columns]
-    # print(new_cols)
-    # df_dummy.columns = new_cols
 
     ####################################################################
     # Import from file directly to xdf - work in progress
@@ -251,7 +287,7 @@ if __name__ == '__main__':
     # #         }
     # #     )
 
-
+    # # Wish to implement these revoscaler functions for revoscalepy:
     # # rxGetInfo(tf, getVarInfo = TRUE, numRows = 10)
     # # rxDataStep(tf, tf, transformFunc = makeDummies, overwrite = TRUE)
     # # rxGetInfo(tf, getVarInfo = TRUE, numRows = 10)
@@ -261,5 +297,5 @@ if __name__ == '__main__':
     # Call main function to work in SQL compute context
     ####################################################################
 
-    main(tablename="Lead_Demography_Tbl_WithID", inputdf=input_df, 
+    main(tablename="Lead_Demography_Tbl", inputdf=input_df, 
         overwrite=False)
